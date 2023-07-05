f26d4096a14fd98e71dfae0be2f4f65270624ec3
vivekgandharla <sonugandherla01@gmail.com>
2023-07-05 22:32:40 +0530

    Initial commit

---
+++ new start/src/com/gfgcip/Main.java
@@ -0,0 +1,15 @@
+package com.gfgcip;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println("your entered number "+ x);
+        double d = sc.nextDouble();
+        System.out.println("your entered number "+ d);
+
+    }
+}
+
