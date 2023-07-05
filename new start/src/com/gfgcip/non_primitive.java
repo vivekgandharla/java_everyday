02c7636c371fcf74511af4d1abf8c85bdffb042f
vivekgandharla <sonugandherla01@gmail.com>
2023-07-05 23:28:12 +0530

    Non_primitive

--- new start/src/com/gfgcip/non_primitive.java
+++ new start/src/com/gfgcip/non_primitive.java
@@ -1,4 +1,21 @@
 package com.gfgcip;
 
+import java.awt.*;
+
 public class non_primitive {
+    int x ;
+    int y;
+
+}
+class test{
+    public static void main(String[] args){
+        Point p1 = new Point();
+        p1.x =10; p1.y=20;
+        System.out.println(p1);
+        Point p2 = p1;
+        p2.x = 30;
+        System.out.println(p1.x);
+        System.out.println(p2.x);
+
+    }
 }
