0803db8658210c838b8c51dc893727ee0e17a7d5
vivekgandharla <sonugandherla01@gmail.com>
2023-07-15 23:38:47 +0530

    LogicalOper.java

---
+++ new start/src/com/gfgcip/LogicalOper.java
@@ -0,0 +1,17 @@
+package com.gfgcip;
+
+import java.util.Scanner;
+
+public class LogicalOper {
+    public static void main(String[]args){
+        //Logical operators
+        System.out.println("Enter your username and password");
+        String user = "Vivek", pwd = "123";
+        Scanner sc = new Scanner(System.in);
+        String iu = sc.nextLine();
+        String ip = sc.nextLine();
+        if(user.equals(iu)&&pwd.equals(ip)){
+            System.out.println("Welcome");
+        }else System.out.println("Try again");
+    }
+}
