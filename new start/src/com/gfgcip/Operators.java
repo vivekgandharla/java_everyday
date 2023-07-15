4cfe3cb28cca78a94cb7ffe80237ea702f1610ba
vivekgandharla <sonugandherla01@gmail.com>
2023-07-15 23:12:05 +0530

    Operators.java

---
+++ new start/src/com/gfgcip/Operators.java
@@ -0,0 +1,16 @@
+package com.gfgcip;
+
+public class Operators {
+    public static void main(String[]args){
+        int x = 10, y = 20;
+        System.out.println(x+y);
+        System.out.println(x*y);
+        System.out.println(y/x);
+        int z = (x+y*10);
+        System.out.println(z);
+        z=x++;
+        System.out.println(z+" "+ x);
+        z=++x;
+        System.out.println(z+" "+x  );
+    }
+}
