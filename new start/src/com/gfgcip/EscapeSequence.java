7a266099b94e803d2f8f57724eff54fbb0e151e9
vivekgandharla <sonugandherla01@gmail.com>
2023-07-15 23:04:08 +0530

    EscapeSequence.java

---
+++ new start/src/com/gfgcip/EscapeSequence.java
@@ -0,0 +1,8 @@
+package com.gfgcip;
+
+public class EscapeSequence {
+    public static void main(String[]args){
+        System.out.println("C:\\xyz\\abc.txt");
+        System.out.println("Hey i'm \"viv\"");
+    }
+}
