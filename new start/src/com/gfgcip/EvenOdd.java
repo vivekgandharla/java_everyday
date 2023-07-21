9f2965353449e9d3c2ad7c58c20ffae028ac0349
vivekgandharla <sonugandherla01@gmail.com>
2023-07-21 23:31:47 +0530

    EvenOdd.java

---
+++ new start/src/com/gfgcip/EvenOdd.java
@@ -0,0 +1,14 @@
+package com.gfgcip;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number ");
+        int n = sc.nextInt();
+        if(n%2==0){
+            System.out.println("Even");
+        } else System.out.println("Odd");
+    }
+}
